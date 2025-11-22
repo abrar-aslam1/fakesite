c8debb1b3073ff41df4aec79ed65926cdfbe851a
Abrar Aslam <abraraslam@Mac.frontiernet.net>
2025-11-22 14:57:53 -0600

    feat: Make site more unique and appealing

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 document.getElementById('login-form').addEventListener('submit', function(event) {
     event.preventDefault();
-    alert('Congratulations! Your login was successful. Please wait while we transfer your $1,000,000 prize. This may take a few business days.');
+    
+    var progressBar = document.getElementById('progress-bar');
+    var width = 0;
+    var interval = setInterval(function() {
+        if (width >= 100) {
+            clearInterval(interval);
+            alert('Congratulations! Your login was successful. Please wait while we transfer your $1,000,000 prize. This may take a few business days.');
+        } else {
+            width++;
+            progressBar.style.width = width + '%';
+        }
+    }, 20);
 });
 
 function startTimer(duration, display) {
